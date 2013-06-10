810b37d03d602be53708db5eea7b8b7ca8b1198c
Andre Heughebaert <aheugheb@ulb.ac.be>
2013-06-10 15:36:01 +0200

    final Data use Agreement.

--- db/import/origids.sql
+++ db/import/origids.sql
@@ -1,9 +1,9 @@
 # this script change the origid of a source, which impact all source objects.
 
-update cl.fundings set origid= replace(origid, 'mec|', 'mineco|') where origid like 'mec|%';
-update cl.projects set origid= replace(origid, 'mec|', 'mineco|') where origid like 'mec|%';
-update cl.orgunits set origid= replace(origid, 'mec|', 'mineco|') where origid like 'mec|%';
-update cl.people set origid= replace(origid, 'mec|', 'mineco|') where origid like 'mec|%';
-update cl.publications set origid= replace(origid, 'mec|', 'mineco|') where origid like 'mec|%';
+update cl.fundings set origid= replace(origid, 'est_sf|', 'etag|') where origid like 'est_sf|%';
+update cl.projects set origid= replace(origid, 'est_sf|', 'etag|') where origid like 'est_sf|%';
+update cl.orgunits set origid= replace(origid, 'est_sf|', 'etag|') where origid like 'est_sf|%';
+update cl.people set origid= replace(origid, 'est_sf|', 'etag|') where origid like 'est_sf|%';
+update cl.publications set origid= replace(origid, 'est_sf|', 'etag|') where origid like 'est_sf|%';
 
-update cl.sources set origid='mineco', acronym='MINECO', name ='' where id=15;
+update cl.sources set origid='etag', acronym='ETAG' where id=7;
